d11c58cebdf90f3f3578d6053b3faec77cb10734
Fidel <ndayishimiyefidel12@gmail.com>
2023-02-25 14:12:00 +0200

    last commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,4 +23,15 @@
 // @for ($i = 1; $i <= 10; $i++) 
 //     echo $i;
 // @endfor;
-?>
+
+
+
+// $talks="word";
+
+// @if (count($talks) === 1)
+// // There is one talk at this time period.
+// @elseif (count($talks) === 0)
+// // There are no talks at this time period.
+// @else
+// // There are {{ count($talks) }} talks at this time period.
+// @endif
